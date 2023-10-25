09583ab3ec74cfd9578292ad926c9ae965021c54
clayton.castro <cocastro@apoioprodesp.sp.gov.br>
2023-10-25 11:34:53 -0300

    firt commit

---
+++ src/main/java/br/com/project/validator/ValidAnnotationApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/project/validator/ValidAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
